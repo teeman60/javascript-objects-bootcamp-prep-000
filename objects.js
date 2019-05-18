17f4915055ecba80fcc2481565ff54f9cbda8533
Taiye Salami <taiye41@gmail.com>
2019-05-18 04:01:58 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artistName: `songTitle`};
+function updatePlaylist(playlist, artistName, songTitle) {
+    updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+    updatePlaylist(playlist, 'Slowdive', "Alison")
+    updatePlaylist(playlist, 'My Bloody Valentine', "Sometimes")
+  return playlist
+}
+
+//function removeFromPlaylist(playlist, artistName) {
+  //return delete playlist.Slowdive
+//}
